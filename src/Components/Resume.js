87326e237480f10a89ed8be6c90210d5e20a7d11
Shahnawaz Bheda <shahnawazbheda@gmail.com>
2023-10-19 09:45:34 +0530

    Fully Responsive code

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -115,43 +115,48 @@ export default function Resume() {
         return (
           <div className={contentClasses}>
             <div className={contentClasses}>
+
               <div className="container mx-auto px-4 mt-5">
                 <div className="bg-white border rounded-3xl shadow">
                   <div className="text-2xl border-b rounded-t-3xl text-white bg-[#2ECA7F]">
-                    <p className='text-5xl p-5 text-center'>Resume</p>
+                    <p className="text-5xl p-5 text-center">Resume</p>
                   </div>
 
                   <div className="p-5 container mx-5">
-                    <div className="grid grid-cols-2 gap-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                       <div>
-                        <p className='text-2xl font-bold'>Education</p>
-
-                        <a href="#" className="block mt-5 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                        <p className="text-2xl font-bold">Education</p>
+                        <a
+                          href="#"
+                          className="block mt-5 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+                        >
                           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">Msc.IT</h5>
                           <p className="font-normal text-gray-700 dark:text-gray-700">Atmiya University</p>
                         </a>
-
-                        <a href="#" className="block mt-5 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
+                        <a
+                          href="#"
+                          className="block mt-5 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+                        >
                           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">BCA</h5>
                           <p className="font-normal text-gray-700 dark:text-gray-700">H.J. Doshi</p>
                         </a>
-
-
                       </div>
-
                       <div>
-                        <p className='text-2xl font-bold'>Experience</p>
-                        <a href="#" className="block p-6 mt-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
+                        <p className="text-2xl font-bold">Experience</p>
+                        <a
+                          href="#"
+                          className="block mt-5 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+                        >
                           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">Frontend-developer</h5>
                           <p className="font-normal text-gray-700 dark:text-gray-700">ImbueSoft llp</p>
                         </a>
-
-                        <a href="#" className="block p-6 mt-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
+                        <a
+                          href="#"
+                          className="block mt-5 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+                        >
                           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">Web Designer</h5>
                           <p className="font-normal text-gray-700 dark:text-gray-700">ImbueSoft llp</p>
                         </a>
-
-
                       </div>
                     </div>
                   </div>
@@ -163,6 +168,7 @@ export default function Resume() {
       case "services":
         return (
           <div className={contentClasses}>
+
             <div className="container mx-auto px-4 mt-5">
               <div className="bg-white border rounded-3xl shadow">
                 <div className="text-2xl border-b rounded-t-3xl text-white bg-[#2ECA7F]">
@@ -174,11 +180,12 @@ export default function Resume() {
                     My <span style={{ color: "#2ECA7F" }}>Services</span>
                   </p>
 
-                  <div className="grid grid-cols-2 gap-4 mt-5">
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-5">
                     <div>
                       <a
                         href="#"
-                        className="block  p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
+                        className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+                      >
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-900">
                           Front-End developer
                         </h5>
@@ -198,8 +205,8 @@ export default function Resume() {
                     <div>
                       <a
                         href="#"
-                        className="block  p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 "
-                        style={{ height: "100%" }}>
+                        className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100" style={{ height: '100%' }}
+                      >
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-900">
                           Back-End Development
                         </h5>
@@ -222,6 +229,7 @@ export default function Resume() {
       case "portfolio":
         return (
           <div className={contentClasses}>
+
             <div className="container mx-auto px-4 mt-5">
               <div className="bg-white border rounded-3xl shadow">
                 <div className="text-2xl border-b rounded-t-3xl text-white bg-[#2ECA7F]">
@@ -229,12 +237,12 @@ export default function Resume() {
                 </div>
 
                 <div className="p-5">
-                  <div className="grid p-3 grid-cols-2 gap-4 mt-5">
-                    <div className="ms-2">
-                      <img src={medicCare} />
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-5">
+                    <div>
+                      <img src={medicCare} alt="Medic Care" className="w-full h-auto  p-12" />
                     </div>
-                    <div className="ms-2">
-                      <img src={filmNest} />
+                    <div>
+                      <img src={filmNest} alt="Film Nest" className="w-full h-auto  p-12" />
                     </div>
                   </div>
                 </div>
@@ -246,7 +254,7 @@ export default function Resume() {
       case "contact":
         return (
           <div className={contentClasses}>
-            <div className="container mx-auto px-4 mt-5">
+            {/* <div className="container mx-auto px-4 mt-5">
               <div className="bg-white border rounded-3xl shadow">
                 <div className="text-2xl border-b rounded-t-3xl text-white bg-[#2ECA7F]">
                   <p className="text-5xl p-5 text-center">Contact</p>
@@ -300,6 +308,57 @@ export default function Resume() {
                   </div>
                 </div>
               </div>
+            </div> */}
+
+            <div className="container mx-auto px-4 mt-5">
+              <div className="bg-white border rounded-3xl shadow">
+                <div className="text-2xl border-b rounded-t-3xl text-white bg-[#2ECA7F]">
+                  <p className="text-5xl p-5 text-center">Contact</p>
+                </div>
+
+                <div className="p-5">
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-3">
+                    <div className="text-xl">
+                      <p className="text-2xl font-bold">
+                        Get in <span className="text-green-500">Touch</span>
+                      </p>
+                      <p className="fa fa-map-marker mt-5 text-green-500">
+                        <span className="ms-2 font-bold text-black">Bajarangvadi, Rajkot</span>
+                      </p> <br/>
+                      <p className="fa fa-envelope mt-3 text-green-500">
+                        <span className="ms-2 font-bold text-black">shahnawazbheda@gmail.com</span>
+                      </p><br/>
+                      <p className="fa fa-phone mt-3 text-green-500">
+                        <span className="ms-2 font-bold text-black">+91 9558362840</span>
+                      </p><br/>
+                      <p className="fa fa-check mt-3 text-green-500">
+                        <span className="ms-2 font-bold text-black">Freelance Available</span>
+                      </p>
+                    </div>
+
+                    <div>
+                      <p className="text-2xl font-bold">
+                        Contact <span className="text-green-500">Us</span>
+                      </p>
+
+                      <div className="mt-4 md:mt-6 text-2xl md:text-3xl">
+                        <a href="https://twitter.com/BhedaShahnawaz">
+                          <button className="fa fa-twitter w-8 h-8 md:w-10 md:h-10 rounded-full bg-white text-gray-400 mr-1" />
+                        </a>
+                        <a href="https://www.facebook.com/shahnawaz.bheda">
+                          <button className=" fa fa-facebook w-8 h-8 md:w-10 md:h-10 rounded-full bg-white text-gray-400 mx-1" />
+                        </a>
+                        <a href="https://www.instagram.com/_shahnawaz_khatri_29/">
+                          <button className="fa fa-instagram w-8 h-8 md:w-10 md:h-10 rounded-full bg-white text-gray-400 mx-1" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/shahnawaz-bheda-7771141a3/">
+                          <button className="fa fa-linkedin w-8 h-8 md:w-10 md:h-10 rounded-full bg-white text-gray-400 mx-1" />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         );
@@ -310,14 +369,14 @@ export default function Resume() {
 
   return (
     <div className="bg-[#F9F9F9] h-screen">
-      <nav className="bg-white dark-bg-gray-900">
+      {/* <nav className="bg-white dark-bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <b>
             <p className="text-2xl">
               Shahnawaz <span className="text-green-500">Bheda</span>
             </p>
           </b>
-          <div className="hidden w-full md:block md:w-auto">
+          <div className="w-full md:block md:w-auto">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4  rounded-lg md:flex-row md:space-x-8 md:mt-0 md-border-0">
               <li>
                 <button
@@ -357,7 +416,60 @@ export default function Resume() {
             </ul>
           </div>
         </div>
-      </nav>
+      </nav> */}
+      <nav className="bg-white dark-bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <b>
+          <p className="text-2xl">
+            Shahnawaz <span className="text-green-500">Bheda</span>
+          </p>
+        </b>
+        <div className="w-full md:block md:w-auto">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md-border-0">
+            <li>
+              <button
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100"
+                onClick={() => handleNavClick("home")}
+              >
+                Home
+              </button>
+            </li>
+            <li>
+              <button
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100"
+                onClick={() => handleNavClick("resume")}
+              >
+                Resume
+              </button>
+            </li>
+            <li>
+              <button
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100"
+                onClick={() => handleNavClick("services")}
+              >
+                Services
+              </button>
+            </li>
+            <li>
+              <button
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100"
+                onClick={() => handleNavClick("portfolio")}
+              >
+                Portfolio
+              </button>
+            </li>
+            <li>
+              <button
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100"
+                onClick={() => handleNavClick("contact")}
+              >
+                Contact
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
       <div className="container mx-auto px-4 mt-5">
         <CSSTransition in={contentVisible} timeout={300} classNames="slide-content" unmountOnExit>
